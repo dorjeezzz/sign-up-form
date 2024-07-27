792f8bdcfaae1f21b805a7af99c77541ef809ad6
dorjeezzz <dj.namjyal@gmail.com>
2024-07-27 11:28:44 -0600

    Added alert for submitting your info

---
+++ script.js
@@ -0,0 +1,4 @@
+const submit = document.querySelector("#sub");
+submit.addEventListener("click", (e)=>{
+    alert("Form Successfully Submitted!\n(note this is not a real service so nothing happened)");
+});
