4c874b2fb015f5b914cfd4383467521912a8a44a
dorjeezzz <dj.namjyal@gmail.com>
2024-07-27 11:47:51 -0600

    implemented red border color for password

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const submit = document.querySelector("#sub");
+const p1 = document.querySelector("#password");
+const p2 = document.querySelector("#confirm_password");
+
 submit.addEventListener("click", (e)=>{
     alert("Form Successfully Submitted!\n(note this is not a real service so nothing happened)");
 });
+
+
